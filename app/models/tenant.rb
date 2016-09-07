6232b153819e5c2b4d812aa3209502cbb7729d79
injurytime78 <injurytime78@gmail.com>
2016-09-07 06:53:56 +0000

    link invite new member

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,21 +1,22 @@
 class Tenant < ActiveRecord::Base
 
-   acts_as_universal_and_determines_tenant
+  acts_as_universal_and_determines_tenant
   has_many :members, dependent: :destroy
+  
+  validates_presence_of :name
+  validates_uniqueness_of :name
 
-    def self.create_new_tenant(tenant_params, user_params, coupon_params)
+  def self.create_new_tenant(tenant_params, user_params, coupon_params)
+    tenant = Tenant.new(tenant_params)
 
-      tenant = Tenant.new(:name => tenant_params[:name])
-
-      if new_signups_not_permitted?(coupon_params)
-
-        raise ::Milia::Control::MaxTenantExceeded, "Sorry, new accounts not permitted at this time" 
-
-      else 
-        tenant.save    # create the tenant
-      end
-      return tenant
+    if new_signups_not_permitted?(coupon_params)
+      raise ::Milia::Control::MaxTenantExceeded, "Sorry, new accounts not permitted at this time"
+    else
+      tenant.save    # create the tenant
     end
+
+    return tenant
+  end
 
   # ------------------------------------------------------------------------
   # new_signups_not_permitted? -- returns true if no further signups allowed
@@ -43,5 +44,5 @@ class Tenant < ActiveRecord::Base
       #
     end
 
-   
+
 end
